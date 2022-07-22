55ae9a4eeade49e600e7d9be468f03495e6321b1
emgullufsen <ericgullufsen@gmail.com>
2022-07-22 18:27:37 -0400

    extracted list of links to fiction

--- scrape.py
+++ scrape.py
@@ -5,4 +5,12 @@ from bs4 import BeautifulSoup
 url = "https://www.hplovecraft.com/writings/texts/"
 page = requests.get("https://www.hplovecraft.com/writings/texts/")
 parser = BeautifulSoup(page.text, 'html.parser')
-print(page.text)
+uls = parser.find_all('ul')
+fiction_list = uls[0].find_all('ul')[0].find_all('a')
+print(fiction_list)
+
+for book in fiction_list[1:]:
+    name = book.contents[0]
+    link = url + book.get('href')
+    print(name)
+    print(link)
